007cde6df7ddf23de263d0d93b613a63e41da908
hereskira <shakiraregalado@gmail.com>
2025-04-24 17:01:01 +0800

    Push project assets

--- obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/EDP_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/EDP_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EDP_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5a2530f7722c47e5e4d403f0e0310c744e07ac3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dfc9b0d9512201651138c900eaec24b8ee1fde92")]
 [assembly: System.Reflection.AssemblyProductAttribute("EDP_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EDP_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
